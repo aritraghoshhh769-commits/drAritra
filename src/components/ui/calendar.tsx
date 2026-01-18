87728d28a27ee2fc845aea88728de84142502021
Jishu Das <jishudas329@gmail.com>
2026-01-18 14:08:36 +0000

    change the dates which are not selectable and make them visible with col

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -44,10 +45,10 @@ function Calendar({
         day_range_end: "day-range-end",
         day_selected:
           "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
-        day_today: "bg-accent text-accent-foreground",
+        day_today: "bg-accent text-accent-foreground disabled:bg-transparent disabled:text-destructive/60",
         day_outside:
           "day-outside text-muted-foreground aria-selected:bg-accent/50 aria-selected:text-muted-foreground",
-        day_disabled: "text-muted-foreground opacity-50",
+        day_disabled: "text-destructive/60 opacity-50",
         day_range_middle:
           "aria-selected:bg-accent aria-selected:text-accent-foreground",
         day_hidden: "invisible",
