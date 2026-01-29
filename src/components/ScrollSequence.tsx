a8c8d0bd6b6189f37102c3635b7377a2e28d6880
Jishu Das <jishudas329@gmail.com>
2026-01-29 17:43:39 +0000

    flip it vertically 180 degrees in the same position (_for element path_)

--- src/components/ScrollSequence.tsx
+++ src/components/ScrollSequence.tsx
@@ -172,7 +172,7 @@ const MobileHero = () => {
             </p>
         </div>
         <div className="absolute bottom-0 left-0 w-full overflow-hidden leading-[0] z-20">
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" className="relative block h-[65px] w-[calc(100%+1.3px)]">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none" className="relative block h-[65px] w-[calc(100%+1.3px)] -scale-y-100">
                 <path d="M1200 0L0 0 0 66.3c235.4 22.4 480 34.5 720.9 34.5 139.7 0 274.3-3.8 400.2-11C1167.6 85.3 1189.3 82.5 1200 80V0z" className="fill-background"></path>
             </svg>
         </div>
